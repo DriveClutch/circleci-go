4d3ef5ce664a8c9c294cff2f86dc1d2439f4a046
David Hallum <david@driveclutch.com>
2017-07-31 09:46:34 -0400

    Adding build all where there is Dockerfiles [#149413363]

--- tools/build.sh
+++ tools/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -ex
 
-CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags "-s -X main.buildinfobuildtime=$(date '+%Y-%m-%d_%I:%M:%S%p') -X main.buildinfogithash=${CIRCLE_SHA1} -X main.buildinfoversion=${CIRCLE_TAG-latest}"
+for dockerfile in $(find . -name Dockerfile -not -path "./vendor/*" )
+do
+	pkgdir=$(dirname $dockerfile)
+	CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags "-s -X main.buildinfobuildtime=$(date '+%Y-%m-%d_%I:%M:%S%p') -X main.buildinfogithash=${CIRCLE_SHA1} -X main.buildinfoversion=${CIRCLE_TAG-latest}" $pkgdir
+done
