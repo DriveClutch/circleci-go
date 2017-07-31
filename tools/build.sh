463095622cc9a98de3a6a898e9f9782a964781b1
David Hallum <david@driveclutch.com>
2017-07-31 10:01:40 -0400

    Adding executable output for build [#149413363]

--- tools/build.sh
+++ tools/build.sh
@@ -3,5 +3,11 @@
 for dockerfile in $(find . -name Dockerfile -not -path "./vendor/*" )
 do
 	pkgdir=$(dirname $dockerfile)
-	CGO_ENABLED=0 go build -a -installsuffix cgo -ldflags "-s -X main.buildinfobuildtime=$(date '+%Y-%m-%d_%I:%M:%S%p') -X main.buildinfogithash=${CIRCLE_SHA1} -X main.buildinfoversion=${CIRCLE_TAG-latest}" $pkgdir
+	execname="$pkgdir/$(basename $pkgdir)"
+	CGO_ENABLED=0 go build \
+		-a \
+		-installsuffix cgo \
+		-ldflags "-s -X main.buildinfobuildtime=$(date '+%Y-%m-%d_%I:%M:%S%p') -X main.buildinfogithash=${CIRCLE_SHA1} -X main.buildinfoversion=${CIRCLE_TAG-latest}" \
+		-o $execname \
+		$pkgdir
 done
